9231b3c99ee281f1441e187933b309deafb816d3
TerryEHLee <terryehlee@gmail.com>
2024-08-28 16:22:06 +0900

    [feat]: signup 페이지 ui

--- src/pages/signup/ui/page/Page.tsx
+++ src/pages/signup/ui/page/Page.tsx
@@ -1,16 +1,15 @@
-import * as React from "react";
-// import {
-//   Avatar,
-//   Button,
-//   CssBaseline,
-//   TextField,
-//   Link,
-//   Grid,
-//   Box,
-//   Typography,
-//   Container,
-// } from "@mui/material";
-// import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import {
+  Avatar,
+  Button,
+  CssBaseline,
+  TextField,
+  Link,
+  Grid,
+  Box,
+  Typography,
+  Container,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 // import { signUp } from "../../../../features/auth/api/Signup";
 
 export const SignupPage = () => {
@@ -34,76 +33,76 @@ export const SignupPage = () => {
   // };
 
   return (
-    <h1>Sign up</h1>
-    // <Container component='main' maxWidth='xs'>
-    //   <CssBaseline />
-    //   <Box
-    //     sx={{
-    //       marginTop: 8,
-    //       display: "flex",
-    //       flexDirection: "column",
-    //       alignItems: "center",
-    //     }}
-    //   >
-    //     <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-    //       <LockOutlinedIcon />
-    //     </Avatar>
-    //     <Typography component='h1' variant='h5'>
-    //       Sign up
-    //     </Typography>
-    //     <Box component='form' noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-    //       <Grid container spacing={2}>
-    //         <Grid item xs={12}>
-    //           <TextField
-    //             autoComplete='given-name'
-    //             name='nickName'
-    //             required
-    //             fullWidth
-    //             id='nickName'
-    //             label='Nick Name'
-    //             autoFocus
-    //           />
-    //         </Grid>
+    <Container component='main' maxWidth='xs'>
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component='h1' variant='h5'>
+          Sign up
+        </Typography>
+        <Box component='form' noValidate sx={{ mt: 3 }}>
+        {/* <Box component='form' noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}> */}
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                autoComplete='given-name'
+                name='nickName'
+                required
+                fullWidth
+                id='nickName'
+                label='Nick Name'
+                autoFocus
+              />
+            </Grid>
 
-    //         <Grid item xs={12}>
-    //           <TextField
-    //             required
-    //             fullWidth
-    //             id='email'
-    //             label='Email for Login ID'
-    //             name='email'
-    //             autoComplete='email'
-    //           />
-    //         </Grid>
-    //         <Grid item xs={12}>
-    //           <TextField
-    //             required
-    //             fullWidth
-    //             name='password'
-    //             label='Password'
-    //             type='password'
-    //             id='password'
-    //             autoComplete='new-password'
-    //           />
-    //         </Grid>
-    //       </Grid>
-    //       <Button
-    //         type='submit'
-    //         fullWidth
-    //         variant='contained'
-    //         sx={{ mt: 3, mb: 2 }}
-    //       >
-    //         Sign Up
-    //       </Button>
-    //       <Grid container justifyContent='flex-end'>
-    //         <Grid item>
-    //           <Link href='/login' variant='body2'>
-    //             Already have an account? Sign in
-    //           </Link>
-    //         </Grid>
-    //       </Grid>
-    //     </Box>
-    //   </Box>
-    // </Container>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id='email'
+                label='Email for Login ID'
+                name='email'
+                autoComplete='email'
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                name='password'
+                label='Password'
+                type='password'
+                id='password'
+                autoComplete='new-password'
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type='submit'
+            fullWidth
+            variant='contained'
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign Up
+          </Button>
+          <Grid container justifyContent='flex-end'>
+            <Grid item>
+              <Link href='/login' variant='body2'>
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
   );
 };
